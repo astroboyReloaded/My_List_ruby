53bb90f6530edc933f4f31ac95d82b2b9d3974e4
gersonhiggins <gahg.10gerson@gmail.com>
2023-07-20 16:23:46 -0400

    Linters

--- my_list.rb
+++ my_list.rb
@@ -1,12 +1,12 @@
 class MyList
   include Enumerable
 
-  def initialize(list)
-    @list = list
-  end
-
   def initialize(*args)
     @list = [1, 2, 3, 4]
     args.each { |arg| @list << arg }
+  end
+
+  def each(&block)
+    @list.each(&block)
   end
 end
